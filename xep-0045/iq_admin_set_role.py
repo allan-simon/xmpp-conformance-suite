a14695cdd807141badad05890a1ed737ba56e5f2
Allan Simon <simona@gobeta.com.cn>
2014-02-07 01:09:24 +0800

    added a beginning of test to check 'set role' stanza'

---
+++ xep-0045/iq_admin_set_role.py
@@ -0,0 +1,130 @@
+from __future__ import print_function
+import logging
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError
+from sleekxmpp.exceptions import IqTimeout
+
+from sleekxmpp.xmlstream import ET
+ADMIN_NS = "http://jabber.org/protocol/muc#admin"
+
+ROOM_JID = "plop@conference.akario.local"
+SECOND_BOT = "bot_2"
+
+#TODO still need to add little more test to see if the set role
+# is actually effective
+
+class EchoBot(ClientXMPP):
+
+    def __init__(self, jid, password, nick):
+        ClientXMPP.__init__(self, jid, password)
+        self.nick = nick
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler(
+            "muc::%s::got_online" % ROOM_JID,
+            self.participant_online
+        )
+
+        self.add_event_handler(
+            "muc::%s::got_offline" % ROOM_JID,
+            self.participant_offline
+        )
+
+    def participant_online(self, msg):
+        if msg['muc'].getNick() != SECOND_BOT:
+            return
+
+        #TODO: in current version of sleekxmpp (as of january 2014)
+        # the setRole function is buggy, so we have to forge the iq ourself
+        iq = self.makeIqSet()
+        iq['to'] = ROOM_JID
+        query = ET.Element('{%s}query' % ADMIN_NS)
+        item = ET.Element(
+            'item',
+            {
+                'role' : 'moderator',
+                'nick'  : SECOND_BOT
+            }
+        )
+        query.append(item)
+        iq.append(query)
+
+        print(
+            "An Admin set iq with an existing role in item tag  " +
+            "should return a 'result' iq ..." ,
+            sep = ' ',
+            end=''
+        )
+
+        try:
+            stanza = iq.send()
+            print('[pass]')
+
+        except IqError as e:
+            print("[fail]")
+        except IqTimeout:
+            print("[fail]")
+
+        self.send_message(
+            mto=ROOM_JID,
+            mbody="disconnect %s" % SECOND_BOT,
+            mtype='groupchat'
+        )
+        #self.disconnect()
+    def participant_offline(self, presence):
+        if presence['muc'].getNick() == SECOND_BOT:
+            self.disconnect()
+            return
+
+
+
+
+
+    def session_start(self, event):
+        self.get_roster()
+        self.send_presence()
+
+
+        self.plugin['xep_0045'].joinMUC(
+            ROOM_JID,
+            self.nick,
+            wait=True
+        )
+class SecondBot(ClientXMPP):
+
+    def __init__(self, jid, password, nick):
+        ClientXMPP.__init__(self, jid, password)
+        self.nick = nick
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("groupchat_message", self.muc_message)
+
+
+    def session_start(self, event):
+        self.get_roster()
+        self.send_presence()
+
+        self.plugin['xep_0045'].joinMUC(
+            ROOM_JID,
+            self.nick,
+            wait=True
+        )
+
+    def muc_message(self, msg):
+        if msg['body'] == 'disconnect %s' % SECOND_BOT:
+            self.disconnect()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.ERROR,
+        format='%(levelname)-8s %(message)s'
+    )
+
+    xmpp = EchoBot('allan@akario.local', 'plop', "bot_1")
+    xmpp.register_plugin('xep_0045')
+    xmpp.connect()
+    xmpp.process(block=False)
+
+    xmpp2 = SecondBot('psi@akario.local', 'plop', SECOND_BOT)
+    xmpp2.register_plugin('xep_0045')
+    xmpp2.connect()
+    xmpp2.process(block=False)
