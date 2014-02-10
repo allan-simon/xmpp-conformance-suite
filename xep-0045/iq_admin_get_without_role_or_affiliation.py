31cc64da6b63a23264716e888cc8801c8070e9c9
Allan Simon <simona@gobeta.com.cn>
2014-02-10 18:11:43 +0800

    factorized iq_admin_get_without_role_or_affiliation.py  by using utility functions

--- xep-0045/iq_admin_get_without_role_or_affiliation.py
+++ xep-0045/iq_admin_get_without_role_or_affiliation.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
-import logging
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError
 from sleekxmpp.exceptions import IqTimeout
 
 from sleekxmpp.xmlstream import ET
-ADMIN_NS = "http://jabber.org/protocol/muc#admin"
+from config import ADMIN_NS
+
+from ConformanceUtils import init_test_one_bot
 
 class EchoBot(ClientXMPP):
 
@@ -52,12 +53,4 @@ class EchoBot(ClientXMPP):
         self.disconnect()
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.ERROR,
-        format='%(levelname)-8s %(message)s'
-    )
-
-    xmpp = EchoBot('allan@akario.local', 'plop', "bot_1")
-    xmpp.register_plugin('xep_0045')
-    xmpp.connect()
-    xmpp.process(block=False)
+    init_test_one_bot(EchoBot)
